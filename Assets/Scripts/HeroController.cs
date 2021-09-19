86a03ce298d0af5120946bc1529dd8631e9f028f
Ian Higa <finn2252000@gmail.com>
2021-09-19 16:35:01 -0700

    Fixed build push

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class HeroController : MonoBehaviour
 {
     private Rigidbody2D body;
+    private Transform myTransform;
+    private float yAccel;
+    [SerializeField] private float maxVerticalAccel = 10f;
+    [SerializeField] private float maxHorizAccel = 2f;
+    private bool facingLeft = true;
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        myTransform = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        body.AddForce(new Vector2(0, 4.9f));
-        Debug.Log(Time.deltaTime);
+        yAccel += .025f;
+        float hInput = Input.GetAxis("Horizontal");
+        anim.SetFloat("flapSpeed", hInput);
+        body.AddForce(new Vector2(hInput * maxHorizAccel, Mathf.Abs(hInput * maxVerticalAccel)));
+        if((facingLeft && (hInput > 0)) || (!facingLeft && (hInput < 0)))
+        {
+            FlipImage();
+            Debug.Log(facingLeft);
+        }
+    }
+
+    // Flip the Bird
+    void FlipImage()
+    {
+        Vector3 flippedScale = myTransform.localScale;
+        flippedScale.x *= -1;
+        facingLeft = !facingLeft;
+        myTransform.localScale = flippedScale;
     }
 }
